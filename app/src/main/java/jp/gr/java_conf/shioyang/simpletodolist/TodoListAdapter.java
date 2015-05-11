c6db9492e6144d0c00ec6a0858a36094be215b1d
shioyang <shioyang4321@gmail.com>
2015-05-11 17:51:09 +0900

    Implement onEditorAction and onFocusChange for edit text

--- app/src/main/java/jp/gr/java_conf/shioyang/simpletodolist/TodoListAdapter.java
+++ app/src/main/java/jp/gr/java_conf/shioyang/simpletodolist/TodoListAdapter.java
@@ -1,12 +1,15 @@
 package jp.gr.java_conf.shioyang.simpletodolist;
 
 import android.content.Context;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -36,11 +39,41 @@ public class TodoListAdapter extends ArrayAdapter {
 
         EditText todoEditText = (EditText) convertView.findViewById(R.id.todoEditText);
         todoEditText.setText(todoItem.getTodo());
+        setOnEditorAction(todoEditText, todoItem);
+        setOnFocusChange(todoEditText, todoItem);
 
         setUpDownFeature(position, convertView);
 
         return convertView;
     }
+
+    private void setOnEditorAction(final EditText todoEditText, final TodoItem todoItem) {
+        todoEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE || keyEvent.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    String newTodo = todoEditText.getText().toString();
+                    todoItem.setTodo(newTodo);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+    }
+
+    private void setOnFocusChange(final EditText todoEditText, final TodoItem todoItem) {
+        todoEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    String newTodo = todoEditText.getText().toString();
+                    todoItem.setTodo(newTodo);
+                }
+            }
+        });
+    }
+
 
     private void setUpDownFeature(final int position, View convertView) {
         Button upButton = (Button) convertView.findViewById(R.id.upButton);
